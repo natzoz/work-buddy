6b4e7b5be569b0d692d9ac2dac4f8c05368d2936
Berto Gonzalez <eriberto@lclark.edu>
2023-04-08 11:14:35 -0700

    down buttons to switch screens

---
+++ WorkBuddy/WorkBuddy/HomePage.swift
@@ -0,0 +1,59 @@
+//
+//  HomePage.swift
+//  WorkBuddy
+//
+//  Created by cs-488-01 on 4/8/23.
+//
+import SwiftUI
+
+struct TaskPage: View {
+    @State private var Timer = true
+    @State private var Task = false
+    var body: some View{
+            if Timer {
+                VStack{
+                    //TimerPage()
+                    Button("Timer"){
+                        Timer.toggle()
+                        Task.toggle()
+                    }.buttonStyle(.bordered)
+                }
+            }else if Task{
+                VStack{
+                    VStack{
+                        List{
+                            //ForEach(Taskcell){ task in Taskcell()
+                              Taskcell(task: "do code for this")
+                            }
+                        }
+                    }
+                HStack{
+                    Button("Task"){}.buttonStyle(.bordered).bold()
+                    Button("Timer"){
+                        Timer.toggle()
+                        Task.toggle()
+                    }.buttonStyle(.bordered)
+                }
+            }
+            }
+        
+        
+    }
+
+
+struct Taskcell: View{
+    var task: String
+    var body: some View{
+        Text(task)
+        VStack{
+            Text("hello")
+        }
+    }
+}
+
+
+struct TaskPage_Previews: PreviewProvider{
+    static var previews: some View{
+        TaskPage()
+    }
+}
--- WorkBuddy/WorkBuddy/WorkBuddyApp.swift
+++ WorkBuddy/WorkBuddy/WorkBuddyApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct WorkBuddyApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TaskPage()
         }
     }
 }
