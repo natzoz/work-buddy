a7fefb50ea10ac0387c719fead016149f3b26527
natalie <nataliezoz@lclark.edu>
2023-04-08 10:38:23 -0700

    Added timer

--- WorkBuddy/WorkBuddy/TimerPage.swift
+++ WorkBuddy/WorkBuddy/TimerPage.swift
@@ -1,9 +1,20 @@
 import SwiftUI
 
 struct TimerPage: View {
+    @State var timeRemaining = 30
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
+        
         VStack {
-            
+            Text("\(timeRemaining)")
+                .onReceive(timer) { _ in
+                    if timeRemaining > 0 {
+                        timeRemaining -= 1
+                    }
+                }
+            Text("Timer!")
         }
     }
 }
--- WorkBuddy/WorkBuddy/WorkBuddyApp.swift
+++ WorkBuddy/WorkBuddy/WorkBuddyApp.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct WorkBuddyApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TimerPage()
         }
     }
 }
