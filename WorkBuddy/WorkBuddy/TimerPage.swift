2bfe27f4275d6cdfc5c2ab7c6dda7c4f8179e9c6
natalie <nataliezoz@lclark.edu>
2023-04-08 10:59:57 -0700

    Created picker

--- WorkBuddy/WorkBuddy/TimerPage.swift
+++ WorkBuddy/WorkBuddy/TimerPage.swift
@@ -2,12 +2,27 @@ import SwiftUI
 
 struct TimerPage: View {
     @State var timeRemaining = 30
+    @State private var selectedHour = 0
+    @State private var selectedMin = 0
+    
+    var hours = [0, 1, 2]
+    var minutes = [0, 1, 2]
+    
+    
+    
     
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
         
         VStack {
+
+            Picker("Hours", selection: $selectedHour) {
+                ForEach(hours, id: \.self) {
+                    Text($0)
+                }
+            }
+            
             Text("\(timeRemaining)")
                 .onReceive(timer) { _ in
                     if timeRemaining > 0 {
