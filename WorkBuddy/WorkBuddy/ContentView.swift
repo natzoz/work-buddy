24d5b00ec02e713e1e207fb8504d776d2d898c4e
natalie <nataliezoz@lclark.edu>
2023-04-08 10:15:03 -0700

    Crated xcode proj

---
+++ WorkBuddy/WorkBuddy/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WorkBuddy
+//
+//  Created by cs-488-01 on 4/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WorkBuddy/WorkBuddy/WorkBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkBuddyApp.swift
+//  WorkBuddy
+//
+//  Created by cs-488-01 on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct WorkBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
