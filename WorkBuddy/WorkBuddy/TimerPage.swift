7fbfc1a198895235f8e8a56ef4a4d6c18fc91c40
Natalie Zoz <nataliezoz.m@gmail.com>
2023-04-08 21:03:59 -0700

    Updated timer

--- WorkBuddy/WorkBuddy/TimerPage.swift
+++ WorkBuddy/WorkBuddy/TimerPage.swift
@@ -1,54 +1,84 @@
 import SwiftUI
 
 struct TimerPage: View {
-    @State var timeRemaining = 30
     @State private var selectedHour = 0
     @State private var selectedMin = 0
+    @State private var sec = 60
     
     @State private var showSettings = true
     @State private var showTimer = false
     
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    let hourTimer = Timer.publish(every: 3600, on: .main, in: .common).autoconnect()
+    let minTimer = Timer.publish(every: 60, on: .main, in: .common).autoconnect()
+    let secTimer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
-        HStack {
-            if showSettings {
-                Picker("Hours", selection: $selectedHour) {
-                    ForEach(0..<24) {
-                        Text("\($0) hours")
+        Text("Work Buddy").font(.system(size: 35, design: .rounded))
+            .padding(.top, 20)
+        Spacer()
+        VStack {
+            HStack {
+                Spacer()
+                if showSettings {
+                    Picker("Hour", selection: $selectedHour) {
+                        ForEach(0..<24) {
+                            Text("\($0)")
+                        }
                     }
+                    .pickerStyle(.wheel)
+                    Text("hours")
+                    Picker("Min", selection: $selectedMin) {
+                        ForEach(0..<60) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.wheel)
+                    Text("min")
                 }
-                
-                Picker("Min", selection: $selectedMin) {
-                    ForEach(0..<60) {
-                        Text("\($0) min")
-                    }
+                Spacer()
+            }
+            HStack {
+                if showTimer {
+                    Text("\(selectedHour)")
+                        .onReceive(hourTimer) { _ in
+                            if selectedHour > 0 {
+                                selectedHour -= 1
+                            }
+                        }
+                    Text(" : ")
+                    Text("\(selectedMin)")
+                        .onReceive(minTimer) { _ in
+                            if selectedMin > 0 {
+                                selectedMin -= 1
+                            }
+                        }
+                    Text(" : ")
+                    Text("\(sec)")
+                        .onReceive(secTimer) { _ in
+                            if sec > 0 {
+                                sec -= 1
+                            } else if sec == 0 {
+                                sec = 60
+                            }
+                        }
                 }
             }
         }
-        
+        Spacer()
         HStack {
+            Spacer()
             Button("Start") {
                 showSettings = false
                 showTimer = true
             }
-            Button("Stop") {
+            Spacer()
+            Button("Reset") {
                 showSettings = true
                 showTimer = false
             }
+            Spacer()
         }
-        
-        VStack {
-            if showTimer {
-                Text("\(selectedMin)")
-                    .onReceive(timer) { _ in
-                        if selectedMin > 0 {
-                            selectedMin -= 1
-                        }
-                    }
-                Text("Timer!")
-            }
-        }
+        Spacer()
     }
 }
 
